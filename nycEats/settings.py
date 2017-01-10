a4544868c13d521444b604d72c0e5753b326bf40
James Chryssanthacopoulos <James@Jamess-MacBook-Air-2.local>
2017-01-10 15:50:35 -0500

    Included django_tables2 in INSTALLED_APPS

--- nycEats/settings.py
+++ nycEats/settings.py
@@ -30,6 +30,7 @@ DEBUG = True
 
 INSTALLED_APPS = [
     'inspection.apps.InspectionConfig',
+    'django_tables2',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
