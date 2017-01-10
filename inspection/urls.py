e5c9d7ef716cf39c320ab6f1a6d314e3e8e515ba
James Chryssanthacopoulos <James@Jamess-MacBook-Air-2.local>
2017-01-10 15:49:58 -0500

    Updated URLs

--- inspection/urls.py
+++ inspection/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     # ex: /inspection/
-    url(r'^$', views.index, name = 'index'),
-    # url(r'^$', views.IndexView.as_view(), name = 'index'),
+    url(r'^$', views.IndexView.as_view(), name = 'index'),
+    
     # ex: /inspection/1/
-    # url(r'^(?P<inspection_id>[0-9]+)/$', views.detail, name = 'detail'),
+    url(r'^(?P<inspection_id>[0-9]+)/$', views.detail, name = 'detail'),
 ]
