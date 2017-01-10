df94be813f57f68d8d275df9c50b68a88a763bae
James Chryssanthacopoulos <James@Jamess-MacBook-Air-2.local>
2017-01-10 15:40:39 -0500

    Included inspection results with restaurant information

--- inspection/admin.py
+++ inspection/admin.py
@@ -2,5 +2,31 @@ from django.contrib import admin
 
 from .models import Restaurant, InspectionResults
 
-admin.site.register(Restaurant)
-admin.site.register(InspectionResults)
+from django.contrib import admin
+
+class InspectionInline(admin.TabularInline):
+    model = InspectionResults
+    extra = 3
+
+class RestaurantAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('ID', {'fields': ['camis']}),
+        ('Name', {'fields': ['name']}),
+        ('Borough', {'fields': ['boro']}),
+        ('Building', {'fields': ['building']}),
+        ('Street', {'fields': ['street']}),
+        ('Zipcode', {'fields': ['zipcode']}),
+        ('Phone', {'fields': ['phone']}),
+        ('Cuisine', {'fields': ['cuisine']}),
+    ]
+    
+    # Inspection display inline
+    inlines = [InspectionInline]
+    
+    # Display
+    list_display = ('camis', 'name', 'is_thai')
+    
+    # search
+    search_fields = ['name']
+
+admin.site.register(Restaurant, RestaurantAdmin)
