f25726b6e49e6b9adc8bbff59e95d399c9e8a2f3
James Chryssanthacopoulos <James@Jamess-MacBook-Air-2.local>
2017-01-10 16:37:42 -0500

    Removed debug info

--- inspection/management/commands/populatedb.py
+++ inspection/management/commands/populatedb.py
@@ -54,7 +54,6 @@ class Command(BaseCommand):
                 gyear = 1970
             
             # Create restaurant
-            print(camis, name, boro, building, street, zipcode, phone, cuisine)
             newRestaurant, _ = Restaurant.objects.get_or_create(
                 camis = camis,
                 name = name,
