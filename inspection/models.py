8bc91c3ac831f12a2e8d5887d56b9d551a01bcfc
James Chryssanthacopoulos <James@Jamess-MacBook-Air-2.local>
2017-01-10 16:36:31 -0500

    Modified some data field types

--- inspection/models.py
+++ inspection/models.py
@@ -13,8 +13,8 @@ class Restaurant(models.Model):
     building = models.CharField(max_length = 10, default = '0')          # Building number (can contain hypen)
     street   = models.CharField(max_length = 20, default = 'Main St')    # Street name
     zipcode  = models.IntegerField(default = 0)                          # Zip code
-    phone    = models.IntegerField(default = 0)                          # Phone number
-    cuisine  = models.CharField(max_length = 20, default = 'American')   # Cuisine description
+    phone    = models.BigIntegerField(default = 0)                       # Phone number
+    cuisine  = models.CharField(max_length = 50, default = 'American')   # Cuisine description
 
     def __str__(self):
         return self.name
@@ -31,9 +31,9 @@ class InspectionResults(models.Model):
     restaurant = models.ForeignKey(Restaurant, on_delete = models.CASCADE)
     
     # Inspection information
-    inspection_type = models.CharField(max_length = 200, default = '') # Type of inspection
+    inspection_type = models.CharField(max_length = 100, default = '') # Type of inspection
     inspection_date = models.DateField(default = timezone.now())       # Inspection date
-    grade           = models.CharField(max_length = 200, default = '') # Inspection grade (A, B, etc.)
+    grade           = models.CharField(max_length = 20, default = '')  # Inspection grade (A, B, etc.)
     score           = models.IntegerField(default = 0)                 # Inspection score
     grade_date      = models.DateField(default = timezone.now)         # Grade date
 
