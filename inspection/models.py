616dc827f2c912d1e2331e132e4e837812787bee
James Chryssanthacopoulos <James@Jamess-MacBook-Air-2.local>
2017-01-10 17:13:57 -0500

    Modified field parameters

--- inspection/models.py
+++ inspection/models.py
@@ -7,14 +7,14 @@ from django.utils.encoding import python_2_unicode_compatible
 @python_2_unicode_compatible
 class Restaurant(models.Model):
     # Restaurant information
-    camis    = models.IntegerField(unique = True, default = 1)           # Restaurant ID
-    name     = models.CharField(max_length = 50, default = 'Restaurant') # Restaurant name
-    boro     = models.CharField(max_length = 50, default = 'Manhattan')  # Borough
-    building = models.CharField(max_length = 50, default = '0')          # Building number (can contain hypen)
-    street   = models.CharField(max_length = 50, default = 'Main St')    # Street name
-    zipcode  = models.IntegerField(default = 0)                          # Zip code
-    phone    = models.BigIntegerField(default = 0)                       # Phone number
-    cuisine  = models.CharField(max_length = 100, default = 'American')  # Cuisine description
+    camis    = models.IntegerField(unique = True, default = 1)            # Restaurant ID
+    name     = models.CharField(max_length = 100, default = 'Restaurant') # Restaurant name
+    boro     = models.CharField(max_length = 100, default = 'Manhattan')  # Borough
+    building = models.CharField(max_length = 100, default = '0')          # Building number (can contain hypen)
+    street   = models.CharField(max_length = 100, default = 'Main St')    # Street name
+    zipcode  = models.IntegerField(default = 0)                           # Zip code
+    phone    = models.BigIntegerField(default = 0)                        # Phone number
+    cuisine  = models.CharField(max_length = 100, default = 'American')   # Cuisine description
 
     def __str__(self):
         return self.name
@@ -33,7 +33,7 @@ class InspectionResults(models.Model):
     # Inspection information
     inspection_type = models.CharField(max_length = 100, default = '') # Type of inspection
     inspection_date = models.DateField(default = timezone.now)         # Inspection date
-    grade           = models.CharField(max_length = 50, default = '')  # Inspection grade (A, B, etc.)
+    grade           = models.CharField(max_length = 100, default = '') # Inspection grade (A, B, etc.)
     score           = models.IntegerField(default = 0)                 # Inspection score
     grade_date      = models.DateField(default = timezone.now)         # Grade date
 
