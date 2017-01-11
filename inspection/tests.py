82ee9d8cfb2f653ca963f7e9e8ff896a8e60292b
James Chryssanthacopoulos <James@Jamess-MacBook-Air-2.local>
2017-01-10 20:32:04 -0500

    Added view tests

--- inspection/tests.py
+++ inspection/tests.py
@@ -4,7 +4,9 @@ tests.py
 
 from django.test import TestCase
 from .models import Restaurant
+from django.urls import reverse
 
+# Method tests
 class RestaurantMethodTests(TestCase):
     def test_is_thai(self):
         """
@@ -36,3 +38,42 @@ class RestaurantMethodTests(TestCase):
         )
         
         self.assertIs(chinese_restaurant.is_thai(), False)
+
+# View tests
+class RestaurantViewTests(TestCase):
+    def test_index_with_no_restaurants(self):
+        """
+        If no restaurants in database, appropriate message should be displayed
+        """
+        
+        response = self.client.get(reverse('index'))
+        
+        self.assertEqual(response.status_code, 200)
+        
+        self.assertContains(response, 'Can\'t find restaurants')
+        
+        self.assertQuerysetEqual(response.context['recommended_restaurants'], [])
+
+# Display tests
+class RestaurantDisplayTests(TestCase):
+    def test_detail_with_restaurant(self):
+        """
+        The detail view should display the restaurant name
+        """
+        
+        restaurant = Restaurant.objects.create(
+            camis = 1,
+            name = 'Gramercy Tavern',
+            boro = 'Manhattan',
+            building = '42',
+            street = 'E 20th St',
+            zipcode = 10003,
+            phone = 2124770777,
+            cuisine = 'American',
+        )
+        
+        url = reverse('detail', args = (restaurant.id,))
+        
+        response = self.client.get(url)
+        
+        self.assertContains(response, restaurant.name)
